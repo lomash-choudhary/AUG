38d0776d39d64618c48a798fb4d127abe51363d2
lomash-choudhary <lomashchoudhary9812@gmail.com>
2025-05-18 17:44:14 +0530

    fix

--- bin.ts
+++ bin.ts
@@ -5,13 +5,12 @@ const numberOfCPUS = os.cpus().length;
 
 if(cluster.isPrimary){
     console.log(`Master worker is running with pid ${process.pid}`)
+    
     for(let i = 0; i < numberOfCPUS; i++){
         cluster.fork();
     }
-    setInterval(() => {}, 1000)
 } else{
     app.listen(3000, () => {
-        console.log(`Woker with pid : ${process.pid}`)
-        console.log(`App is listening on http://localhost:3000`)
+        console.log(`Woker with pid : ${process.pid} started`)
     })
 }
